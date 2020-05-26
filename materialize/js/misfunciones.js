0998d5dc68c685e34c0e75bcefd3dd0f0c02b509
HEVER <hevershtp@gmail.com>
2020-05-26 14:48:58 -0500

    MI primer proyecto Materialize

---
+++ materialize/js/misfunciones.js
@@ -0,0 +1,17 @@
+
+$( document ).ready(function() {
+    $(".dropdown-trigger").dropdown();
+});
+$(document).ready(function(){
+    $('.carousel').carousel();
+  });
+  $(document).ready(function(){
+    $('.collapsible').collapsible();
+  });
+  
+  $(document).ready(function(){
+    $('.tabs').tabs();
+    $('.modal').modal();
+  });
+
+        
